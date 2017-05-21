5033557b9fdf4b7f8d03ef939f184491a9b78d28
Hossein <m.h.askari.hemmat@gmail.com>
2017-05-21 13:45:29 -0400

    refactoring project with the requested project name

--- mkChiselProj.sh
+++ mkChiselProj.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Exit if error exist
+set -eu
+
 #Defining color variables:
 CSTART='\e[0;32m'
 Green='\033[0;32m'
@@ -23,34 +27,41 @@ function show_help()
 }
 
 # Read input args and clone git repo:
-if [ "$1" == "help" ]; then
+if [[ "$#" -eq 0 ]]; then
+	echo -e "${Red}Cloning $BASE_URL to a default project name...${CEND}"
+elif [[ "$1" == "help" ]] 
+then
 	show_help
 	exit 1
-fi
-
-if [ "$1" != "" ]; then
-	PROJ_NAME="$1"
 else
-	echo -e "${Red}Cloning $BASE_URL to a default project name...${CEND}"
+	PROJ_NAME="$1"
 fi
 
-if [ -d "$PROJ_NAME" ]; then
+
+if [[ -d "$PROJ_NAME" ]]; then
 	echo -e "${Red}$PROJ_NAME directory already exist! try to create this project in different directory. ${CEND}"	
 	exit 1
 fi
-
-PROJ_PATH=$PROJ_DIR/$PROJ_NAME
-IDEA_DIR=$PROJ_PATH/.idea
-sed -i 's/SimpleReg/$PROJ_NAME/g' $IDEA_DIR/
 
 echo -e "Cloning ${Green}$BASE_URL${CEND} repository in ${Green}$PROJ_NAME${CEND} directory..."
 git clone "$BASE_URL" "$PROJ_NAME"
 cd "$PROJ_NAME"
 rm -rf .git 
+
+# Rename idea project
+PROJ_PATH=$PROJ_DIR/$PROJ_NAME
+IDEA_DIR=$PROJ_PATH/.idea
+sed -i "s/SimpleReg/${PROJ_NAME}/g" $IDEA_DIR/workspace.xml
+cd $PROJ_PATH/src/test/scala/
+mv SimpleRegUnitTest.scala ${PROJ_NAME}UnitTest.scala 
+sed -i "s/SimpleReg/${PROJ_NAME}/g" *.scala
+cd $PROJ_PATH/src/main/scala/
+mv SimpleReg.scala ${PROJ_NAME}.scala 
+sed -i "s/SimpleReg/${PROJ_NAME}/g" *.scala
+
+# init git repo and add all files
+cd $PROJ_PATH
 git init
+git add *
 
 echo "done!"
-
-
-echo $PROJ_PATH
-echo $IDEA_DIR
